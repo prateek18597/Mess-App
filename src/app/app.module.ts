04adab47104a28b48720b63548f6efa522aa2df8
Pratik Parmar <prateek18597@gmail.com>
2018-12-08 20:35:45 +0530

    LoginPage completed

---
+++ src/app/app.module.ts
@@ -0,0 +1,61 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+
+import { AboutPage } from '../pages/about/about';
+import { ContactPage } from '../pages/contact/contact';
+import { HomePage } from '../pages/home/home';
+import { TabsPage } from '../pages/tabs/tabs';
+import { LoginPage } from '../pages/login/login';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AuthProvider } from '../providers/auth/auth';
+
+const config = {
+    apiKey: "AIzaSyDcpdxSh88qKeB8lk6cmIDy5VY9JNs8A-4",
+    authDomain: "mess-2a5dd.firebaseapp.com",
+    databaseURL: "https://mess-2a5dd.firebaseio.com",
+    projectId: "mess-2a5dd",
+    storageBucket: "mess-2a5dd.appspot.com",
+    messagingSenderId: "377309961021"
+  };
+
+@NgModule({
+  declarations: [
+    MyApp,
+    AboutPage,
+    ContactPage,
+    HomePage,
+    TabsPage,
+    LoginPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(config),
+    AngularFireAuthModule
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    AboutPage,
+    ContactPage,
+    HomePage,
+    TabsPage,
+    LoginPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    AngularFireAuthModule,
+    AuthProvider
+  
+  ]
+})
+export class AppModule {}
